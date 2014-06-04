ea8e84b36f8ea34fa9cc6fda1d3bed46652f6ca2
Hermann <herm@Hermann-Fas-MacBook-Pro.local>
2014-06-04 09:05:18 +0200

    Restructured to a central NameCorrector class.

--- lib/namecorrector.rb
+++ lib/namecorrector.rb
@@ -2,24 +2,8 @@
 
 require 'pp'
 require 'yaml'
-require 'singleton'
 
-class Surname < String
-
-    attr_accessor :title
-
-    def initialize( surname )
-        super
-    end
-
-    def to_proper_case()
-        SurnameCorrector.instance.correct_surname( self )
-    end
-
-end
-
-class SurnameCorrector
-    include Singleton
+class NameCorrector
 
     DataDir = File.join( File.dirname(__FILE__), 'data' )
 
@@ -31,6 +15,12 @@ class SurnameCorrector
     attr_reader   :titles
     attr_reader   :split_regexp
 
+    # Return a new NameCorrector instance.
+    #--
+    # The Regexp with other_pattern does not catch letters beyond ASCII.
+    # Worse, it considers them as word boundaries.
+    # Implement something here or in the stringextension package.  
+    #++
     def initialize()
         @guess_camel_case = true
         @guess_prepositions = true
@@ -39,14 +29,14 @@ class SurnameCorrector
             @titles = YAML.load(f)
         end
         File.open( File.join(DataDir, 'prepositions.yaml') ) do |f|
-            @prepositions = YAML.load(f)
+            @prepositions = YAML.load( f )
         end
         File.open( File.join(DataDir, 'irregular_surnames.yaml') ) do |f|
             @irregular_surnames = YAML.load( f )
         end
         title_pattern = @titles.join('|')
         preposition_pattern = @prepositions.join('|')
-        other_pattern= '\w+'
+        other_pattern= '[-\w]+'
         @split_regexp = Regexp.new(
             '(?:' +
             [title_pattern, preposition_pattern, other_pattern].join('|') +
@@ -54,23 +44,33 @@ class SurnameCorrector
             Regexp::IGNORECASE )
     end
 
-    def trivial_proper_case(str)
-        # Test wise now the trivial way:
-        words = []
-        str.split(/[\s]+/).each do |word|
-            words << word.capitalize
-        end
-        words.join(' ')
+    # Capitalize every word of a string, treating the part
+    # after a hyphen as a new word.
+    #--
+    # To do: 
+    # Change regular expressinos to POSIX or find other way to handle
+    # Umlaut, accents, and perhaps other non-ASCII letters correctly.
+    # Perhaps the upcase method for String should go to 
+    # stringextension.rb or so.
+    #++
+    def capitalize_name(n)
+        corrected_name = ''
+        n.scan(/\w+[ -]?/).each { |p| corrected_name << p.capitalize } 
+        corrected_name
     end
 
-    def correct_surname(surname)
+    # Change the uppercase and lowercase usage in a name according to
+    # the typical conventions in German, English, and some other European
+    # languages. However, character sets beyond ASCII are not handled
+    # properly in the current version. 
+    def make_proper_case(n)
         corrected_parts = []
-        parts = surname.scan( @split_regexp )
+        parts = n.scan( @split_regexp )
         parts.each do |p|
             if ( @prepositions.include?(p) )
                 corrected_parts << p
             else 
-                corrected_parts << ( irregular_surnames[p] || p.capitalize )
+                corrected_parts << ( irregular_surnames[p] || self.capitalize_name(p) )
             end
         end
         corrected_parts.join(' ')
@@ -78,5 +78,5 @@ class SurnameCorrector
 
 end
 
-sn = Surname.new('Hermann ANdreAS von den dipasquale')
-puts sn.to_proper_case
+sn = 'Hermann-ANdreAS von den dipasquale-Migräne'
+puts NameCorrector.new.make_proper_case(sn)
